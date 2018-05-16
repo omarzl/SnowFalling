559b7e11470d36698fca3425884e7432d798bc04
omarzl <omarzl@hotmail.es>
2018-05-15 23:32:35 -0500

    setups the initial flakes

--- SnowFalling/SnowFallingView.swift
+++ SnowFalling/SnowFallingView.swift
@@ -30,6 +30,10 @@ public class SnowFallingView: UIView {
     public var animationDurationMax: Float = 0
     
     public var flakesArray = [UIImageView]()
+    public var initialFlakesArray = [UIImageView]()
+    
+    public var shouldStartFromTop = false
+    private var initialSetup = true
     
     override public init(frame: CGRect) {
         super.init(frame: frame)
@@ -42,18 +46,20 @@ public class SnowFallingView: UIView {
         self.flakeMaximumSize     = kDefaultMaximumSize
         self.animationDurationMin = kDefaultAnimationDurationMin
         self.animationDurationMax = kDefaultAnimationDurationMax
+        
+        backgroundColor = .clear
     }
-
+    
     public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
     
-    public func createFlakes() {
-        flakesArray = [UIImageView]()
+    public func createFlakes(count: Int) -> [UIImageView] {
+        var array = [UIImageView]()
         guard let flakeImage = UIImage(named: flakeFileName) else {
-            return
+            return array
         }
-        for _ in 0..<flakesCount {
+        for _ in 0..<count {
             var vz: Float = 1.0 * Float(arc4random()) / Float(RAND_MAX)
             vz = vz < flakeMinimumSize ? flakeMinimumSize : vz
             vz = vz > flakeMaximumSize ? flakeMaximumSize : vz
@@ -70,16 +76,21 @@ public class SnowFallingView: UIView {
             let imageView = UIImageView(image: flakeImage)
             imageView.frame = CGRect(x: CGFloat(vx), y: CGFloat(vy), width: CGFloat(vw), height: CGFloat(vh))
             imageView.isUserInteractionEnabled = false
-            flakesArray.append(imageView)
+            array.append(imageView)
             addSubview(imageView)
         }
+        return array
     }
     
     public func startSnow() {
-        if flakesArray.isEmpty {
-            createFlakes()
+        if !shouldStartFromTop, initialSetup {
+            initialFlakes()
         }
-        backgroundColor = .clear
+        initialSetup = false
+        
+        if flakesArray.isEmpty {
+            flakesArray = createFlakes(count: flakesCount)
+        }
         
         let rotAnimation: CABasicAnimation = CABasicAnimation(keyPath: "transform.rotation.y")
         rotAnimation.repeatCount = Float.infinity
@@ -108,12 +119,54 @@ public class SnowFallingView: UIView {
         }
     }
     
+    public func initialFlakes() {
+        if initialFlakesArray.isEmpty {
+            initialFlakesArray = createFlakes(count: 50)
+        }
+        
+        let rotAnimation: CABasicAnimation = CABasicAnimation(keyPath: "transform.rotation.y")
+        rotAnimation.repeatCount = Float.infinity
+        rotAnimation.autoreverses = false
+        rotAnimation.toValue = 6.28318531
+        rotAnimation.isRemovedOnCompletion = false
+        
+        let theAnimation: CABasicAnimation = CABasicAnimation(keyPath: "transform.translation.y")
+        theAnimation.autoreverses = false
+        theAnimation.isRemovedOnCompletion = false
+        theAnimation.delegate = self
+        
+        for v in initialFlakesArray {
+            let startypos = Int(arc4random_uniform(UInt32(frame.size.height)))
+            v.center.y = frame.size.height
+            let timeInterval: Float = (animationDurationMax - animationDurationMin) * Float(arc4random()) / Float(RAND_MAX)
+            theAnimation.duration = CFTimeInterval(timeInterval + animationDurationMin) * 0.5
+            theAnimation.fromValue = -startypos
+            v.layer.add(theAnimation, forKey: "transform.translation.y")
+            
+            rotAnimation.duration = CFTimeInterval(timeInterval)
+            v.layer.add(rotAnimation, forKey: "transform.rotation.y")
+        }
+    }
+    
     public func stopSnow() {
         for v in flakesArray {
             v.layer.removeAllAnimations()
-            v.removeFromSuperview()
         }
         flakesArray.removeAll()
+    }
+}
+
+extension SnowFallingView : CAAnimationDelegate {
+    public func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
+        for image in initialFlakesArray {
+            if anim == image.layer.animation(forKey: "transform.translation.y") {
+                image.removeFromSuperview()
+                if let index = initialFlakesArray.index(of: image) {
+                    initialFlakesArray.remove(at: index)
+                }
+                return
+            }
+        }
     }
 }
 
